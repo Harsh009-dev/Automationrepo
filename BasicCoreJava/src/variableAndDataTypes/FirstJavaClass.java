a5133343d63ec9b354ba934b2dd9d462f7a8db84
Harsh <harshalpatil009@gmail.com>
2020-01-25 12:54:55 +0530

    Committing First Java Class

---
+++ BasicCoreJava/src/variableAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,11 @@
+package variableAndDataTypes;
+
+public class FirstJavaClass {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the world of Automation");
+	}
+
+}
